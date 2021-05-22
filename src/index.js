3356ad63d835011c246e69c289a305e44c00d197
Monika Wasilewska <monikawasilewska13@wp.pl>
2021-05-22 21:26:42 +0200

    fetching data

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
-<ProductsProvider>
-    <App/>
+    <ProductsProvider>
+        <App/>
     </ProductsProvider>, 
 document.getElementById('root'))
