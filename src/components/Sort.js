fa2907323f290d0df1a888948cba86465faf1fd5
Monika Wasilewska <monikawasilewska13@wp.pl>
2021-07-25 21:55:46 +0200

    adding filters

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -41,8 +41,8 @@ const Sort = () => {
           value={sort}
           onChange={updateSort}
         >
-          <option value="price-lowest">price(lowest)</option>
-          <option value="price-highest">price(highest)</option>
+          <option value="price-lowest">price (lowest)</option>
+          <option value="price-highest">price (highest)</option>
           <option value="name-a">name(a-z)</option>
           <option value="name-z">name(z-a)</option>
         </select>
