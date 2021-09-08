6496bb432fd2731f9cdbf8a3c9fe59e3b94d6c7a
Monika Wasilewska <monikawasilewska13@wp.pl>
2021-09-08 15:30:17 +0200

    adding stripe

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-bp6iui9m.eu.auth0.com"
-    clientId="gOjHf6cWDpcvocQWndas89xwZ1VqT6Id"
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation="localstorage"
   >
